074cf3413158cef102e9531c8be7a86a957956b7
supasuma <supasuma@gmail.com>
2016-10-16 13:51:57 +0100

    aws regions

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,7 @@ config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :s3_region => ENV['AWS_REGION'],
+    :s3_region => ENV['AWS_REGION']
   }
 }
 end
